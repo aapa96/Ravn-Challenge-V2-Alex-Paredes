ca403fa794c7a00d694f9027e19834345c66ac19
AlexParedes <aapa96@gmail.com>
2021-02-12 03:35:54 -0500

    fix deploy to heroku

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 
 const server = require('./server');
 const http = require('http').createServer(server);
-const path = require('path'); 
+const path = require('path');
 const PORT = 3000;
-http.listen((PORT), (error) => {
-    if (error) return console.log(error) 
-    console.log(`Server started on port ${PORT}`);
+server.listen(3000, (error) => {
+    if (error) return console.log(error)
 });
